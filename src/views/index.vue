5db8807c9688fce9d9b94423455d6257635fe64b
kammy1996 <kamranmemon25@gmail.com>
2023-02-09 13:50:59 +0530

    vuetify installed

---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+    <v-container>
+      new vue
+    </v-container>
+   </div>
+</template>
+<script>
+export default {
+  name: 'MainComponent',
+  data() {
+    return {
+    }
+  },
+}
+</script>
+<style lang='scss' scoped>
+</style>
